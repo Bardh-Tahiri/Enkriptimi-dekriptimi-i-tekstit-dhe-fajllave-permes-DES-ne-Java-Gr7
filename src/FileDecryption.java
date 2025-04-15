9df49e7dfcc90734a149cb2d07f84e212c05bb8d
Bardh-Tahiri <bardhtahiri15@gmail.com>
2025-04-15 19:57:58 +0200

    Krijimi i FileEncryption dhe FileDecryption

---
+++ src/FileDecryption.java
@@ -0,0 +1,5 @@
+public class FileDecryption {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/FileEncryption.java
@@ -0,0 +1,5 @@
+public class FileEncryption {
+    public static void main(String args[]){
+
+    }
+}
