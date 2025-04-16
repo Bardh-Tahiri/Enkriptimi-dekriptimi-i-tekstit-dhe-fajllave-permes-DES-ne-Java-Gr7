68cbd2f04ab1b9504b4186d6c0cd0697f16d03f7
Aurelahasanaj <aurela.hasanaj@student.uni-pr.edu>
2025-04-16 19:15:34 +0200

    Gjenerimi i celsit dhe ruajtja e celsit ne hex

--- src/FileDecryption.java
+++ src/FileDecryption.java
@@ -5,22 +5,6 @@ import java.io.*;
 import java.util.Formatter;
 public class FileDecryption {
     public static void main(String[] args) {
-        try{
-            // 1. Gjenerimi i Celesit ne DES
-            KeyGenerator keyGen = KeyGenerator.getInstance("DES");
-            keyGen.init(56); // DES-i perdor celsa 56-bit
-            SecretKey secretKey = keyGen.generateKey();
 
-            // 2. Ruajtja e Celsit Heksadecimal ne nje file (Opcionale)
-            byte[] keyBytes = secretKey.getEncoded();
-            StringBuilder keyHex = new StringBuilder();
-            Formatter keyFormatter = new Formatter(keyHex);
-            for (byte b : keyBytes) {
-                keyFormatter.format("%02X", b);
-            }
-            keyFormatter.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
